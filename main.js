fe24e83982e10ca61ad2ee673dd0bcf8ca46d1d1
henrique912 <sabinohenrique912@gmail.com>
2023-08-02 22:10:41 -0300

    Site funcionando

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-function pesquisacep(valor) {
+function buscarEndereco() {
+    var cep = document.getElementById("cepInput").value;
+    var url = `https://viacep.com.br/ws/${cep}/json/`;
 
-
-    function PesquisarCep(){
-        let dado = document.getElementById("cep").value
-        replace("-","");
-        ColetarCep(dados);
-     }
-
-     //Buscar pelo cep na API da VIACEP e retornar no formato obejeto json.
-     async function ColetarCep(dados){
-    
-        //Biblioteca fetch é usada para buscar a API ou WebService e amazena como objeto.
-     const cep =await fetch('URL: viacep.com.br/ws/${dados}/json/')
-     .then(
-
-        Response => Response.json()
-     );
-      }
-      DadosTela(cep)
-    }
-
-//Enviar os dados retornando para o html
-
-function DadosTela(cep){
-
-    console.log(cep)
-    document.getElementById('rua').value=(conteudo.logradouro);
-    document.getElementById('bairro').value=(conteudo.bairro);
-    document.getElementById('cidade').value=(conteudo.localidade);
-    document.getElementById('uf').value=(conteudo.uf);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (!data.erro) {
+                exibirResultado(data);
+            } else {
+                exibirMensagemErro("CEP não encontrado");
+            }
+        })
+        .catch(error => {
+            exibirMensagemErro("Erro na consulta do CEP");
+            console.error(error);
+        });
 }
 
-     
-        
+function exibirResultado(data) {
+    var resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = `
+        <p><strong>CEP:</strong> ${data.cep}</p>
+        <p><strong>Logradouro:</strong> ${data.logradouro}</p>
+        <p><strong>Bairro:</strong> ${data.bairro}</p>
+        <p><strong>Cidade:</strong> ${data.localidade} - ${data.uf}</p>
+    `;
+}
 
-  
+function exibirMensagemErro(mensagem) {
+    var resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = `<p class="erro">${mensagem}</p>`;
+}
